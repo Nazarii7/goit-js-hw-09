1a7b17890866ade86e1192e8efc843bbd64591ef
Nazarii7 <nazariipushkaruk@gmail.com>
2022-08-28 05:13:29 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const startB = document.querySelector('[data-start]');
-const stopB = document.querySelector('[data-stop]');
+// const stopB = document.querySelector('[data-stop]');
 const clockFace = document.querySelector('timer');
 const dayV = document.querySelector('[data-days]');
 const hoursV = document.querySelector('[data-hours]');
@@ -41,73 +41,75 @@ function timerOn(finDate) {
     timer.start(utcFinDate, currentT);
   });
 
-  stopB.addEventListener('click', () => {
-    timer.stop();
-  });
-}
+  //   stopB.addEventListener('click', () => {
+  //     timer.stop();
+  //   });
+  // }
 
-class Timer {
-  constructor({ onTick }) {
-    this.intervalId = null;
-    this.isActive = false;
-    this.onTick = onTick;
-    this.i();
-  }
-  i() {
-    const time = this.convertMs(0);
-    this.onTick(time);
-  }
-
-  start(finTime, currentT) {
-    if (this.isActive) {
-      return;
+  class Timer {
+    constructor({ onTick }) {
+      this.intervalId = null;
+      this.isActive = false;
+      this.onTick = onTick;
+      this.i();
     }
-    this.isActive = true;
-    this.intervalId = setInterval(() => {
-      const currentT = Date.now();
-      const deltaT = finTime - currentT;
-      const time = this.convertMs(deltaT);
-
+    i() {
+      const time = this.convertMs(0);
       this.onTick(time);
-      this.timeS(deltaT);
-    }, 1000);
-  }
-  timeS(deltaT) {
-    if (deltaT < 1000) {
-      timer.stop();
-      Notify.failure('NOT GOOD!');
+    }
+
+    start(finTime, currentT) {
+      if (this.isActive) {
+        return;
+      }
+      this.isActive = true;
+      this.intervalId = setInterval(() => {
+        const currentT = Date.now();
+        const deltaT = finTime - currentT;
+        const time = this.convertMs(deltaT);
+
+        this.onTick(time);
+        this.timeS(deltaT);
+      }, 1000);
+    }
+    timeS(deltaT) {
+      if (deltaT < 1000) {
+        timer.stop();
+        Notify.failure('NOT GOOD!');
+      }
+    }
+    // stop() {
+    //   clearInterval(this.intervalId);
+    //   this.isActive = false;
+    //   const time = this.convertMs(0);
+    //   this.onTick(time);
+    // }
+
+    convertMs(ms) {
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const days = Math.floor(ms / day);
+      const hours = Math.floor((ms % day) / hour);
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      return { days, hours, minutes, seconds };
     }
   }
-  stop() {
-    clearInterval(this.intervalId);
-    this.isActive = false;
-    const time = this.convertMs(0);
-    this.onTick(time);
+  function pad(value) {
+    return String(value).padStart(2, '0');
   }
 
-  convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const timer = new Timer({ onTick: updateTimerFace, });
 
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
+  function updateTimerFace({ days, hours, minutes, seconds }) {
+    dayV.textContent = `${days}`;
+    hoursV.textContent = `${hours}`;
+    minutesV.textContent = `${minutes}`;
+    secondsV.textContent = `${seconds}`;
   }
-}
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
 
-const timer = new Timer({ onTick: updateTimerFace });
-
-function updateTimerFace({ days, hours, minutes, seconds }) {
-  dayV.textContent = `${days}`;
-  hoursV.textContent = `${hours}`;
-  minutesV.textContent = `${minutes}`;
-  secondsV.textContent = `${seconds}`;
-}
+  
